3cbe910b9055d7b88ce61bc7c828063c5e4d9cdc
Daniel Valsby-Koch <dava@aspit.dk>
2018-05-24 13:26:26 +0200

    Whoops. Here is the former ignored StatsCreator

---
+++ DirFileStats.Business/StatsCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirFileStats.Business
+{
+    public static class StatsCreator
+    {
+        public static FileStats CreateFileStats(System.IO.FileInfo fileInfo)
+        {
+            //TODO: Extract the relevant info from the parameter and create a FileStats object
+            //Extra comment
+            throw new NotImplementedException();
+        }
+
+        public static DirectoryStats CreateDirectoryStats(System.IO.DirectoryInfo directoryInfo)
+        {
+            //TODO: Extract the relevant info from the parameter and create a DirectoryStats object
+            throw new NotImplementedException();
+        }
+    }
+}
