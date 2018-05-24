1b46b81961b8c82439ec1588e1e8a7207a6aa998
Daniel Valsby-Koch <dava@aspit.dk>
2018-05-24 11:53:07 +0200

    See the other commit message

--- DirFileStats.UserInterface/MainWindow.xaml.cs
+++ DirFileStats.UserInterface/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace DirFileStats.UserInterface
 {
@@ -23,6 +25,64 @@ namespace DirFileStats.UserInterface
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnChooseFile_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new CommonOpenFileDialog()
+            {
+                Title = "Vælg en mappe eller fil",
+                IsFolderPicker = false,
+                AddToMostRecentlyUsedList = false,
+                AllowNonFileSystemItems = false,
+                EnsureFileExists = true,
+                EnsurePathExists = true,
+                EnsureReadOnly = false,
+                EnsureValidNames = true,
+                Multiselect = false,
+                ShowPlacesList = true
+            };
+            if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                // TODO:
+                // Show the path (found in dlg.FileName) in the correct textbox so the user can see what was chosen
+
+                FileInfo fileInfo = new FileInfo(dlg.FileName);
+
+                // TODO:
+                // Use the fileInfo to create a FileStats object and show the relevant data for the user
+            }
+
+
+        }
+
+        private void btnChooseDir_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new CommonOpenFileDialog()
+            {
+                Title = "Vælg en mappe eller fil",
+                IsFolderPicker = true,
+                AddToMostRecentlyUsedList = false,
+                AllowNonFileSystemItems = false,
+                EnsureFileExists = true,
+                EnsurePathExists = true,
+                EnsureReadOnly = false,
+                EnsureValidNames = true,
+                Multiselect = false,
+                ShowPlacesList = true
+            };
+            if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                // TODO:
+                // Show the path (found in dlg.FileName) in the correct textbox so the user can see what was chosen
+
+                DirectoryInfo dirInfo = new DirectoryInfo(dlg.FileName);
+
+                // TODO:
+                // Use the dirInfo to create a DirectoryStats object and show the relevant data for the user
+            }
+
+
         }
     }
 }
